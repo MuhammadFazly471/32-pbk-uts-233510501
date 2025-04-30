0cb6b168faac494914efcee22f786e4d9f823f56
MuhammadFazly471 <fazlyadzhar471@gmail.com>
2025-04-30 15:14:10 +0700

    fitur add task and display

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <script setup>
+import { ref } from 'vue'
+
+const tasks = ref([])
+const newTask = ref('')
+
+const addTask = () => {
+  tasks.value.push({
+    id: tasks.value.length + 1,
+    title: newTask.value,
+    completed: false
+  })
+  newTask.value = ''
+}
 
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Testing</h1>
+  <div>
+    <input type="text" v-model="newTask" @keyup.enter="addTask">
+    <button @click="addTask">Add Task</button>
+  </div>
+
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }}
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <style scoped>
